2ff469a57039dd6743fdd1241bb76567f74c44b3
Kevin <kevinarmandzakka@gmail.com>
2020-01-04 18:24:00 +0200

    added class method to extract pointcloud and save as ply

--- fusion.py
+++ fusion.py
@@ -298,6 +298,27 @@ class TSDFVolume:
       cuda.memcpy_dtoh(self._color_vol_cpu, self._color_vol_gpu)
     return self._tsdf_vol_cpu, self._color_vol_cpu
 
+  def get_point_cloud(self):
+    """Extract a point cloud from the voxel volume.
+    """
+    tsdf_vol, color_vol = self.get_volume()
+
+    # Marching cubes
+    verts = measure.marching_cubes_lewiner(tsdf_vol, level=0)[0]
+    verts_ind = np.round(verts).astype(int)
+    verts = verts*self._voxel_size + self._vol_origin
+
+    # Get vertex colors
+    rgb_vals = color_vol[verts_ind[:, 0], verts_ind[:, 1], verts_ind[:, 2]]
+    colors_b = np.floor(rgb_vals / self._color_const)
+    colors_g = np.floor((rgb_vals - colors_b*self._color_const) / 256)
+    colors_r = rgb_vals - colors_b*self._color_const - colors_g*256
+    colors = np.floor(np.asarray([colors_r, colors_g, colors_b])).T
+    colors = colors.astype(np.uint8)
+
+    pc = np.hstack([verts, colors])
+    return pc
+
   def get_mesh(self):
     """Compute a mesh from the voxel volume using marching cubes.
     """
@@ -375,3 +396,30 @@ def meshwrite(filename, verts, faces, norms, colors):
     ply_file.write("3 %d %d %d\n"%(faces[i,0], faces[i,1], faces[i,2]))
 
   ply_file.close()
+
+
+def pcwrite(filename, xyzrgb):
+  """Save a point cloud to a polygon .ply file.
+  """
+  xyz = xyzrgb[:, :3]
+  rgb = xyzrgb[:, 3:].astype(np.uint8)
+
+  # Write header
+  ply_file = open(filename,'w')
+  ply_file.write("ply\n")
+  ply_file.write("format ascii 1.0\n")
+  ply_file.write("element vertex %d\n"%(xyz.shape[0]))
+  ply_file.write("property float x\n")
+  ply_file.write("property float y\n")
+  ply_file.write("property float z\n")
+  ply_file.write("property uchar red\n")
+  ply_file.write("property uchar green\n")
+  ply_file.write("property uchar blue\n")
+  ply_file.write("end_header\n")
+
+  # Write vertex list
+  for i in range(xyz.shape[0]):
+    ply_file.write("%f %f %f %d %d %d\n"%(
+      xyz[i, 0], xyz[i, 1], xyz[i, 2],
+      rgb[i, 0], rgb[i, 1], rgb[i, 2],
+    ))
